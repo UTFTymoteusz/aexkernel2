75336496c02d0d53e28168ae369fedee6d755cb8
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-06-02 21:30:37 +0200

    that code was apparently too fast

--- arch/x64/string.cpp
+++ arch/x64/string.cpp
@@ -4,10 +4,11 @@
 #include <stdint.h>
 
 namespace AEX {
-    void memcpy(void* dst, const void* src, size_t size) {
+    // rep movsq didn't work properly
+    /*void memcpy(void* dst, const void* src, size_t size) {
         size_t aligned = size / 8;
 
-        asm volatile("mov rcx, %2; rep movsq;" : : "D"(dst), "S"(src), "r"(aligned));
+        asm volatile("cld; mov rcx, %2; rep movsq;" : : "D"(dst), "S"(src), "r"(aligned));
 
         size_t remainder = size - aligned * 8;
 
@@ -15,21 +16,21 @@ namespace AEX {
         uint8_t* src8 = (uint8_t*) src + aligned * 8;
 
         asm volatile("mov rcx, %2; rep movsb;" : : "D"(dst8), "S"(src8), "r"(remainder));
-    }
+    }*/
 
     void memset(void* mem, char c, size_t len) {
-        asm volatile("mov al, %1; mov rcx, %2; rep stosb;" : : "D"(mem), "r"(c), "r"(len));
+        asm volatile("cld; mov al, %1; mov rcx, %2; rep stosb;" : : "D"(mem), "r"(c), "r"(len));
     }
 
     void memset16(void* mem, uint16_t n, size_t count) {
-        asm volatile("mov ax, %1; mov rcx, %2; rep stosd;" : : "D"(mem), "r"(n), "r"(count));
+        asm volatile("cld; mov ax, %1; mov rcx, %2; rep stosd;" : : "D"(mem), "r"(n), "r"(count));
     }
 
     void memset32(void* mem, uint32_t n, size_t count) {
-        asm volatile("mov eax, %1; mov rcx, %2; rep stosd;" : : "D"(mem), "r"(n), "r"(count));
+        asm volatile("cld; mov eax, %1; mov rcx, %2; rep stosd;" : : "D"(mem), "r"(n), "r"(count));
     }
 
     void memset64(void* mem, uint64_t n, size_t count) {
-        asm volatile("mov rax, %1; mov rcx, %2; rep stosq;" : : "D"(mem), "r"(n), "r"(count));
+        asm volatile("cld; mov rax, %1; mov rcx, %2; rep stosq;" : : "D"(mem), "r"(n), "r"(count));
     }
 }
